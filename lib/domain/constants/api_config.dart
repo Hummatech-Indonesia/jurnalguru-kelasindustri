86d7586338af0aaec35362ece4a335de8d96bd91
Tio Irawan <sattanationmail@gmail.com>
2024-08-07 09:26:48 +0700

    chore: change base url

--- lib/domain/constants/api_config.dart
+++ lib/domain/constants/api_config.dart
@@ -1,10 +1,5 @@
-import 'package:flutter/foundation.dart';
-
 abstract class ApiConfig {
-  // static const String _devBaseUrl = "https://classhummatech.mijurnal.com";
-  static const String _devBaseUrl = "https://class.hummatech.com";
-  static const String _prodBaseUrl = "https://class.hummatech.com";
-  static String baseUrl = kDebugMode ? _devBaseUrl : _prodBaseUrl;
+  static const String baseUrl = "https://class.hummatech.com";
   static String apiUrl = "$baseUrl/api";
   static String storageUrl = "$baseUrl/storage";
 }
